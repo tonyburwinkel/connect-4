facb997f47e9776d984a8b92361d8521ffee5cbe
tonyburwinkel <burwinkel.a@northeastern.edu>
2023-05-23 22:21:44 -0400

    made model object oriented. abstracted functions into class methods

--- js/model.js
+++ js/model.js
@@ -1,5 +1,7 @@
-
+// a class that provides all the functionality
+// of a game of connect 4
 class Connect4Model{
+
     // create an instance of connect4
     constructor(rows, columns){
         this.name = "connect 4";
@@ -15,12 +17,13 @@ class Connect4Model{
         for (let i = 0; i < columns; i++) {
         board[i] = [];
         for (let j = 0; j < rows; j++) {
-            board[i][j] = null; // You can change the initial value as needed
+            board[i][j] = null; 
             }
         }
         return board;
     }
 
+    // Returns a string version of the board for debugging
     printBoard() {
         let columns = this.board.length;
         let rows = this.board[0].length;
@@ -38,34 +41,43 @@ class Connect4Model{
     }
 
     // return whether a position in the board is occupied
-    isOccupied(board, col, row) {
-        if (board[col][row])return true;
+    isOccupied(col, row) {
+        if (this.board[col][row])return true;
         return false;
     }
 
     // play a piece given a column 
     // should check if move is valid before calling move
+    // returns final position of piece
     move(column) {
         console.log('moving')
         this.board[this.boardCounts[column]][column] = this.players[this.currentPlayer];
+        this.boardCounts[column]++;
+        return [column, boardCounts[column-1]];
     }
 
     // return whether a column is full
     isFull(column) {
-        if (board[column].every(e=>e!==null)) return true;
+        if (this.boardCounts[column]===rows) return true;
         return false;
     }
 
+    // change the current player's turn
     switchTurn() {
         this.currentPlayer++;
         this.currentPlayer%=2;
     }
+
+    checkWin(column, row, count){    
+    };
 }
 
 const main = () => {
     let c4game = new Connect4Model(6, 10);
     console.log(c4game.printBoard());
     c4game.move(5);
+    c4game.switchTurn();
+    c4game.move(6);
     console.log(c4game.printBoard());
 }
 
