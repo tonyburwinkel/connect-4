bd89f2736fbf0794bc2665de8951e721d06bada2
tonyburwinkel <burwinkel.a@northeastern.edu>
2023-05-31 20:30:36 -0400

    rudimentary view elements added to index.html

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
     console.log('howdy pard');
 
-    let rows=
+    for(let i=0;i<10;i++){
+        let column = $(`<div id="col-${i}"></div>`);
+        column.addClass('column');
+        for(let j=0;j<6;j++){
+            let space = $(`<div id="space-${j}"></div>`);
+            space.addClass('space');
+            column.append(space);
+        }
+        column.click((e)=>{
+            console.log(`${i}`);
+            })
+        $("#game-container").append(column);
 
-    $(".game-container").attr('background', 'black')
+    }
 
 })
