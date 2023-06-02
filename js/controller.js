f14301dfa4d793870f8564d368fabf8e2276f5c3
tonyburwinkel <burwinkel.a@northeastern.edu>
2023-06-02 18:51:14 -0400

    hover row working as intended

--- js/controller.js
+++ js/controller.js
@@ -2,26 +2,20 @@ $(document).ready(function(){
     c4 = new window.Connect4Model(6, 10);
 
     // construct the board
-    for(let i=0;i<10;i++){
-        let column = $(`<div id="col-${i}"></div>`);
-        column.addClass('column');
-        for(let j=6;j>=0;j--){
-            if(j===6){
-                let space = $(`<div id="c-${i}r-${j}"></div>`);
-                space.addClass('hover-space');
-                column.append(space);
-            } else {
-            let space = $(`<div id="c-${i}r-${j}"></div>`);
-            space.addClass('space');
-            column.append(space);
-            }
-        }
-        // add a click listener to each row that
-        // makes a move on the board and renders the move
-        column.click(()=>{
-            if (c4.isValidMove(i)) {
-                let move=c4.move(`${i}`);
-                let selected=$(`#c-${move[0]}r-${move[1]}`);
+    for(let i=6;i>=0;i--){
+        let row = $(`<div id="row-${i}"></div>`);
+        i===6?
+            row.addClass('hover-row'):
+            row.addClass('row');
+        for(let j=0;j<10;j++){
+            let space = $(`<div id="c-${j}r-${i}"></div>`);
+            i===6?
+                space.addClass('hover-space'):
+                space.addClass('space');
+            space.click(()=>{
+                if (c4.isValidMove(j)){
+                    let move=c4.move(`${j}`);
+                    let selected=$(`#c-${move[0]}r-${move[1]}`);
                     switch(c4.getPlayer()) {
                         case 'r':
                             selected.css('background-color', 'red');
@@ -30,15 +24,14 @@ $(document).ready(function(){
                             selected.css('background-color', 'black');
                             break;
                     }
-                console.log(c4.printBoard());
-                c4.switchTurn();
-                let winner=c4.checkWin(move);
-                console.log(`winner: ${winner}`);
-                if(winner) $('#header').append(`<p>${winner} wins!</p>`);
-                }else console.log('invalid move');
+                    console.log(c4.printBoard());
+                    c4.switchTurn();
+                    let winner=c4.checkWin(move);
+                    if(winner) $('#header').append(`<p>${winner} wins!</p>`);
+                } else console.log('invalid move');
             })
-        $("#game-container").append(column);
-
-    }
-
+            row.append(space);
+            }
+        $("#game-container").append(row);
+        }
 })
